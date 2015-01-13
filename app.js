9fa98fb1f7ebb64e4046ec23b7532df109fd56d3
ndouthett <nick.douthett@gmail.com>
2015-01-13 22:14:53 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	var firstNum =  parseInt(prompt("Enter a number: "));
+   numCheck(firstNum);
+   
+});
+
+function fizzBuzz(firstNum) {
+   
+for (var i = 1; i <= firstNum; i++) {
+
+		if (i % 15 === 0) {
+			$('.results').append('FizzBuzz');
+
+		}
+        else if (i % 3 === 0) {
+			$('.results').append('Fizz');
+
+		} 
+        else if (i % 5 === 0) {
+			$('.results').append('Buzz');
+
+		} 
+        else {
+			$('.results').append(i);
+		}
+		$('.results').append('<br />');
+	}
+}
+
+function numCheck(firstNum) {
+    if (firstNum%1 != 0 || firstNum == 0 || firstNum[0] == " ") {
+		alert("Please try again with an integer number.");
+    }else
+        fizzBuzz(firstNum);
+}
+
+
+
+
+
+    
